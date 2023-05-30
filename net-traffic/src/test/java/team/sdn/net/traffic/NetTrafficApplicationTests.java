5c2a0519530d407bbca0867b6c069b6d96e1fa53
TokisakiKurumi <2105721136@qq.com>
2023-05-30 10:52:01 +0800

    feat:添加net-traffic模块

---
+++ net-traffic/src/test/java/team/sdn/net/traffic/NetTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package team.sdn.net.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetTrafficApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
