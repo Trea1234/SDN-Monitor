cf4b719fd755ad179880b1a89d106874bf13d6c6
steveevens <3236805730@qq.com>
2023-06-01 20:45:56 +0800

    to():工具模块实体类填充

---
+++ net-utils/src/main/java/team/sdn/domain/Flow.java
@@ -0,0 +1,29 @@
+package team.sdn.domain;
+
+import java.util.Date;
+
+/**
+ * 接口流量
+ * @author Big-Bai
+ **/
+public class Flow {
+    /**
+     *传输的字节数
+     */
+    private Long transmittedBytes;
+
+    /**
+     * 接收的字节数
+     */
+    private Long receivedBytes;
+
+    /**
+     *
+     */
+    private Long transmittedPkts;
+
+    /**
+     *
+     */
+    private Long receivedPkts;
+}
--- net-utils/src/main/java/team/sdn/sdnutils/FlowTableUtils.java
+++ net-utils/src/main/java/team/sdn/sdnutils/FlowTableUtils.java
@@ -8,13 +8,15 @@ import java.util.List;
  * 流表相关工具
  * @author Big-Bai
  **/
-public interface FlowTableUtils {
+public class FlowTableUtils {
     /**
      * 批量下发流表
      * @param flow 流表
      * @return 是否下发成功
      */
-    Boolean addFlowsBatch(OpenFlow flow);
+    public static Boolean addFlowsBatch(OpenFlow flow){
+        return null;
+    }
 
     /**
      * 为某个交换机下发流表
@@ -22,20 +24,26 @@ public interface FlowTableUtils {
      * @param flow 流表内容
      * @return 是否下发成功
      */
-    Boolean addFlowForSwitch(Integer switchId,OpenFlow flow);
+    public static Boolean addFlowForSwitch(Integer switchId,OpenFlow flow){
+        return null;
+    }
 
     /**
      * 获取所有的流表
      * @return 所有的流表
      */
-    List<OpenFlow> getAllFlowTables();
+    public static List<OpenFlow> getAllFlowTables(){
+        return null;
+    }
 
     /**
      * 获取某个交换器的流表
      * @param switchId 交换机id
      * @return 流表们
      */
-    List<OpenFlow> getAllTablesOfSwitch(Integer switchId);
+    public static List<OpenFlow> getAllTablesOfSwitch(Integer switchId){
+        return null;
+    }
 
     /**
      * 删除某个交换机的某个流表
@@ -43,13 +51,17 @@ public interface FlowTableUtils {
      * @param tableId 流表id
      * @return 是否成功删除
      */
-    Boolean deleteFlowTable(Integer switchId,Integer tableId);
+    public static Boolean deleteFlowTable(Integer switchId,Integer tableId){
+        return null;
+    }
     /**
      * 删除某个交换机的所有流表
      * @param switchId 交换机id
      * @return 是否成功删除
      */
-    Boolean deleteAllTableOfSwitch(Integer switchId);
+    public static Boolean deleteAllTableOfSwitch(Integer switchId){
+        return null;
+    }
 
     /**
      * 修改某个交换机的某个流表内容
@@ -57,7 +69,9 @@ public interface FlowTableUtils {
      * @param tableId 流表id
      * @return 是否成功删除
      */
-    Boolean modifyFlowTable(Integer switchId,Integer tableId);
+    public static Boolean modifyFlowTable(Integer switchId,Integer tableId){
+        return null;
+    }
 
 
 }
