4de46ba744b1f08e88b96aaba267972b8a39d5b2
侯瑞宁 <94113626+xiaomougui@users.noreply.github.com>
2023-06-01 21:30:44 +0800

    Feat:start test

---
+++ ruoyi-ui/src/views/test/common/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { testFun } from "@/api/test/common/index.js";
+export default {
+  name: "test",
+  data() {
+    return {
+      url: "ws://192.168.0.113:8888/websocket",
+      protol: "",
+      wsData: {
+        token: "token-123456",
+        data: "666",
+      },
+    };
+  },
+  mounted() {
+    testFun(url, protol, wsData);
+  },
+};
+</script>
+
+<style>
+</style>
