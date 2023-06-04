829d2584341305ca654fbbd78992f7bd69314bcb
steveevens <3236805730@qq.com>
2023-06-04 22:09:47 +0800

    feat(Util:test):添加拓扑测试

---
+++ net-utils/src/test/java/TopologyUtilsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import team.sdn.domain.Topology;
+import team.sdn.sdnutils.TopologyUtils;
+
+/**
+ * @author Big-Bai
+ **/
+public class TopologyUtilsTest {
+    @Test
+    public void getTopologyTest(){
+        System.out.println(TopologyUtils.getTopology().toString());
+    }
+}
